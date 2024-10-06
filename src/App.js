0e1d59bb6e30370d1ae6d086136a4e45fc776aa0
NathalieMireles <natrodriguez628@gmail.com>
2024-10-06 17:40:09 -0600

    el intento se hizo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'; 
 
 function App() {
+  
+  const [numeros, setNumeros] = useState([]); 
+
+  const agregarNumero = () => {
+    setNumeros([...numeros, 0]); 
+  };
+
+  const manejarCambioNumero = (indice, valor) => {
+    const nuevosNumeros = [...numeros]; 
+    nuevosNumeros[indice] = valor ? Number(valor) : 0; 
+    setNumeros(nuevosNumeros); 
+  };
+
+ 
+  const eliminarNumero = (indice) => {
+    const nuevosNumeros = numeros.filter((_, i) => i !== indice); 
+    setNumeros(nuevosNumeros); 
+  };
+
+  const total = numeros.reduce((acc, curr) => acc + curr, 0);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Total: {total}</h1>
+        <button onClick={agregarNumero}>Agregar Número</button> 
+        <div className="inputs-container">
+          {numeros.map((numero, indice) => (
+            <div key={indice}>
+              <input
+                type="number"
+                value={numero}
+                onChange={(e) => manejarCambioNumero(indice, e.target.value)}
+              />
+              <button onClick={() => eliminarNumero(indice)}>Eliminar Numero</button> 
+            </div>
+          ))}
+        </div>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
